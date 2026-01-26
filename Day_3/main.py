bc5b5b7066cdf5912660c705c07864caa5ca1dc7
Abhi Changela <abhichangela@gmail.com>
2026-01-26 16:18:13 +0530

    String examples

---
+++ Day_3/main.py
@@ -0,0 +1,12 @@
+print("Test's this");
+print('Test"s this');
+print('Test\'s this');
+print('Test\'s "this"');
+print('Test' + ' Test');
+print(10 * 'Test ');
+print('C:\docs\next');
+# To resolve above problem we need to use raw string
+print(r'C:\docs\next');
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+print("Python is fun!") print("Really!")
