f68d69ca91ecbf710b5f5af4b54518a2d147b6ce
Abhi Changela <abhichangela@gmail.com>
2026-01-27 17:00:33 +0530

    Strings in python

---
+++ Day_4/main.py
@@ -0,0 +1,19 @@
+x = 10;
+# print(x);
+
+x=2;
+y=4;
+z=x+y
+print(z);
+
+a = 'Hello World!!';
+print(a);
+print(a[0] + a[10]);
+print(a[-3]);
+# print(a[20]);
+print(a[1:5]);
+print(a[3:]);
+print(a[:5]);
+print('Test' + a[5:]);
+a = 'Test';
+print(a);
