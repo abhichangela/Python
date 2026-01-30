2f527991e0d3ef2f0b7707d2009bf85bac683c41
Abhi Changela <abhichangela@gmail.com>
2026-01-30 09:56:35 +0530

    set in python

---
+++ Day_7/main.py
@@ -0,0 +1,13 @@
+# SET
+# Set is a collection of unique elements.
+# To define the set, we use the {} curly brackets.
+# Set never follows the sequence.
+# When we print the set, then the sequence of values in output will be different from the sequence of input.
+# Duplicate values present in a set will be printed only once in an output.
+# Set uses the concept of Hash. Hash is used to improve the performance as it fetches the values as fast as possible.
+# Indexing is not supported in sets as it does not follow sequencing.
+# Values can not be changed in a set also because index value is not supported.
+
+abc = {11, 35, 8, 20, 55, 11};
+print(abc);
+# print(abc[2]); #This cannot work
