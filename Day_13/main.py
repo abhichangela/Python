f5b07dbffe108bdde600d79116e6e85056a5e26b
Abhi Changela <abhichangela@gmail.com>
2026-02-04 11:17:04 +0530

    Operators in python

---
+++ Day_13/main.py
@@ -0,0 +1,59 @@
+x=3;
+y=4;
+print(x+y, x-y, x*y, x/y);
+
+x= x+2;
+print(x);
+y+=3;
+print(y);
+
+a,b = 5,7;
+print(a,b);
+print(a<b, a>b);
+print(a == b);
+print(a <= b);
+print(a != b);
+
+n = 10;
+-n
+print(-n, "---", n);
+n = -n;
+print(-n, "---", n);
+
+p = 9;
+q = 4;
+print(p<10 and q<8);
+print(p<10 and q<2);
+print(p<10 or q<8);
+print(p<10 or q<2);
+
+x = True;
+print(x, "----", not x);
+
+
+# Different types of operators in Python
+# Use of different operators
+# Operation performed between different operators
+# Logical operators and their uses
+
+# Types of operators in Python:
+# 1. Arithmetic operators
+# 2. Assignment operators
+# 3. Relational operators
+# 4. Logical operators
+# 5. Unary operators
+# Arithmetic operators are used to perform mathematical operations like addition, subtraction, multiplication, division, etc.
+# Assignment operation is performed with the help of equal to(=), in which we can assign a value to the variable.
+# We can also do the assignment by short-hand which means performing an arithmetic operation and then assigning the value both at the same time.
+# We can also assign the values in one line for two variables.
+#  a,b = 5,6
+# Unary operator takes a single operand in an expression or a statement.
+# Comparison can be performed with the help of relational operators.
+# Comparison operators return a boolean value or True or False.
+
+# If you want to combine two or more conditions and then check the relation between them, then logical operators are used.
+# Logical operators include And, Or and Not.
+# Logical operators follow the truth table for And,  Or, and Not operators.
+# And the operator returns True when both conditions are true otherwise returns False. 
+# Or the operator returns True when any of the conditions or both the conditions are True otherwise it returns False.
+# Not the operator reverses the value of the output.
