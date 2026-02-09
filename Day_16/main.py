340d36ed6a792c6018a099cfcf92b49544d9e73c
Abhi Changela <abhichangela@gmail.com>
2026-02-09 16:59:06 +0530

    Math default function

---
+++ Day_16/main.py
@@ -0,0 +1,40 @@
+import math;
+print(math.sqrt(49));
+print(math.floor(3.7));
+print(math.ceil(3.3));
+print(math.pow(2,3));
+print(math.pi);
+print(math.e);
+
+# -- math module in Python provides a wide range of mathematical functions.
+# -- math module is a built-in module in Python.
+# -- math module is used to perform mathematical operations.
+# -- math module is used to perform mathematical operations like square root, power, floor, ceil, etc. 
+
+# -- for using math module we have to import it first 
+# import math
+# -- then we can use math module function like math.sqrt(25) 
+# or
+# -- we can use alias name like m=math then we can use m. 
+#  m.sqrt(25) if we import math as m
+# or 
+# -- we can import only required function like from math import sqrt,pow
+# sqrt(25)
+# pow(2,3)
+
+# Here are some of the most commonly used ones methods of math module:
+
+# math.sqrt(x): Returns the square root of x.
+# math.pow(x, y): Returns x raised to the power y.
+# math.ceil(x): Returns the smallest integer greater than or equal to x.
+# math.floor(x): Returns the largest integer less than or equal to x.
+# math.exp(x): Returns the exponential value of x.
+# math.log(x[, base]): Returns the natural logarithm (base e) of x or the logarithm of x with the specified base.
+# math.sin(x), math.cos(x), math.tan(x): Returns the trigonometric sine, cosine, or tangent of x, respectively.
+
+# Here are some of the most commonly used ones attributes(constant) of math module:
+
+# math.pi: Returns the value of pi.
+# math.e: Returns the value of e.
+# math.inf: Returns a floating point positive infinity.
+# math.nan: Returns a floating point “not a number” (NaN) value.
